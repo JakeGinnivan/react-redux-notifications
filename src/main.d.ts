882ef9921c4f7356d883bdffc3bdc03a2d5ddc8d
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2018-09-08 18:52:37 +0800

    Fix type definitions

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,43 +1,39 @@
-declare module "react-redux-notifications" {
-  import * as React from "react";
-  import * as redux from "redux";
+import * as React from "react";
+import * as redux from "redux";
 
-  export interface Notification {
-    key: string;
-    message: string;
-    trigger: redux.Action;
-    showDismiss: boolean;
-  }
-
-  export interface ListeningTo {
-    hideAfter?: number;
-    defaultMessage?: string;
-    showDismiss: boolean;
-  }
-
-  export interface NotificationsState {
-    listeningTo: {
-      [event: string]: ListeningTo[];
-    };
-    notifications: {
-      [event: string]: Notification[];
-    };
-  }
-
-  export interface InlineNotificationProps {
-    triggeredBy: string | string[];
-    defaultMessage?: string;
-    hideAfter?: number;
-    renderNotification?: (
-      notification: Notification,
-      dismiss: (e?: MouseEvent) => void
-    ) => JSX.Element;
-    renderContainer?: (notifications: JSX.Element[]) => JSX.Element;
-  }
-
-  export const reducer: redux.Reducer<NotificationsState>;
-  export const middleware: redux.Middleware;
-  export const InlineNotification: React.ComponentClass<
-    InlineNotificationProps
-  >;
+export interface Notification {
+  key: string;
+  message: string;
+  trigger: redux.Action;
+  showDismiss: boolean;
 }
+
+export interface ListeningTo {
+  hideAfter?: number;
+  defaultMessage?: string;
+  showDismiss: boolean;
+}
+
+export interface NotificationsState {
+  listeningTo: {
+    [event: string]: ListeningTo[];
+  };
+  notifications: {
+    [event: string]: Notification[];
+  };
+}
+
+export interface InlineNotificationProps {
+  triggeredBy: string | string[];
+  defaultMessage?: string;
+  hideAfter?: number;
+  renderNotification?: (
+    notification: Notification,
+    dismiss: (e?: MouseEvent) => void
+  ) => JSX.Element;
+  renderContainer?: (notifications: JSX.Element[]) => JSX.Element;
+}
+
+export const reducer: redux.Reducer<NotificationsState>;
+export const middleware: redux.Middleware;
+export const InlineNotification: React.ComponentClass<InlineNotificationProps>;
